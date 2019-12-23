6e4d5a9f0f3b5d1d0dfcdf56f3192e4108054f9e
johncode101 <john.carrera911@gmail.com>
2019-12-23 05:20:44 -0800

    Updated app with firebase installed.

---
+++ IntagramFirebase/Utils/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  IntagramFirebase
+//
+//  Created by Jonathan Hernandez on 12/23/19.
+//  Copyright © 2019 Jonathan Hernandez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+    //    func someRandomeMethod() {
+    //
+    //    }
+    
+}
